b37c8a293d668415736945438b51b1d74e4638af
haidichen <chenhaidi43@163.com>
2019-06-27 10:47:10 -0500

    modified and fixed some existing issues

--- flask/apis/objdetect_api/run.sh
+++ flask/apis/objdetect_api/run.sh
@@ -7,4 +7,13 @@ else
 	mkdir images
 fi
 
+if [ -e 'yolo-coco/yolov3.weights' ]
+then
+		echo 'service ready'
+else
+		echo '!! Cannot find file: yolo-coco/yolov3.weights !!'
+		echo 'Service ABORTED'
+		exit 1
+fi
+
 python3 app.py
